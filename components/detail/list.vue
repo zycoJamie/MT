baf299e2184818ed9c72b829b613b5ef219c12b4
Levi <410752387@qq.com>
2019-04-08 12:27:17 +0800

    产品详情页-页面开发

---
+++ components/detail/list.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="m-detail-list">
+        <ul>
+            <li>{{list.filter(item=>item.photos.length>0).length}}款套餐</li>
+            <item v-for="(item,idx) in list" :meta="item" :key="idx"/>
+        </ul>
+    </div>
+</template>
+    
+<script>
+import Item from '@components/detail/item'
+export default {
+    components:{
+        Item
+    },
+    prpos:{
+        list:{
+            type:Array,
+            default:[]
+        }
+    }
+}
+</script>
+    
+<style lang='scss'>
+    
+</style>
---
+++ pages/detail.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="page-detail">
+      <el-row>
+          <el-col :span="24">
+              <crumbs :keyword="keyword"
+              :type="type"/>
+          </el-col>
+      </el-row>
+      <el-row>
+          <el-col :span="24">
+              <summa :meta="product"/>
+          </el-col>
+      </el-row>
+      <el-row>
+          <el-col :span="24">
+              <h3>商家团购及优惠</h3>
+          </el-col>
+      </el-row>
+      <el-row>
+          <el-col :span="24">
+              <list :list="list">
+          </el-col>
+      </el-row>
+  </div>
+</template>
+
+<script>
+import Crumbs from '@components/detail/crumbs'
+import Summa from '@components/detail/summary'
+import List from '@components/detail/list'
+export default {
+    components:{
+        Crumbs,Summa,List
+    },
+    data(){
+        return {
+            keyword:'',
+            type:'',
+            product:{},
+            list:[]
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+@import "@/assets/css/detail/index.scss";
+</style>
